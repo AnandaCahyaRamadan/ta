14ee3de6b06a922bd7f615a5e25a358eef88717b
AnandaCahyaRamadan <76020880+AnandaCahyaRamadan@users.noreply.github.com>
2023-06-08 13:03:02 +0700

    [FIX] Title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Mimi Kitty</title>
     
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
