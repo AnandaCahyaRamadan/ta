b80fcbdcaf109a921d3f9759b5cbf93cc9969c3f
AnandaCahyaRamadan <76020880+AnandaCahyaRamadan@users.noreply.github.com>
2023-06-09 09:53:18 +0700

    [FIX] Card height

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -133,40 +133,41 @@
         
         <section id="products">
           @if (count($products) > 0 && $products[0]['status'] === 'approved')
-            <div class="container pt-4 pb-5">
-                <div class="row g-4">
-                  @foreach ($products as $key => $product)
-                    @if ($product->status == 'approved')
-                      <div class="col-12 col-sm-6 col-md-3">
-                          <div class="card" style="width: 100%;">
-                              <img class="card-img-top" src="{{ asset('storage/' . $product->gambar )}}" alt="Card image cap">
-                              <div class="card-body">
-                                <h5 class="card-title my-1">{{ $product->nama_product }}</h5>
-                                <small class="fs-xmall fw-bold bg-warning pe-2 ps-2 pb-1 pt-1 rounded">{{ $product->categories->category_name }}</small>
-                                <p class="card-text my-1 text-warning text-bold">Rp. {{ $product->harga }}</p>
-                                <small class="card-text">{{ $product->deskripsi }}</small>
-                                <div class="mt-2">
-                                  @for ($i = 1 ; $i <= $product->rating ; $i++)
-                                  <i class="fa fa-star text-warning"></i>
-                                  @endfor
-                                  <span> {{ $product->rating }}/5</span>
-                                </div>
-                                <div class="mt-4">
-                                  <a href="https://wa.me/6281999651534" class="btn btn-warning fw-bold" style="width:100%"><i class="fab fa-whatsapp"></i> Pesan Sekarang</a>
-                                </div>
-                              </div>
-                          </div>
+          <div class="container pt-5 pb-5">
+            <div class="row g-4">
+              @foreach ($products as $key => $product)
+              <div class="col-12 col-sm-6 col-md-3 d-flex align-items-stretch">
+                <div class="card" style="width: 100%;">
+                  <div class="d-flex flex-column h-100">
+                    <img class="card-img-top" src="{{ asset('storage/' . $product->gambar )}}" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title my-1">{{ $product->nama_product }}</h5>
+                      <small class="fs-xmall fw-bold bg-warning pe-2 ps-2 pb-1 pt-1 rounded">{{ $product->categories->category_name }}</small>
+                      <p class="card-text my-1 text-warning text-bold">Rp. {{ $product->harga }}</p>
+                      <small class="card-text">{{ $product->deskripsi }}</small>
+                      <div class="mt-2">
+                        @for ($i = 1 ; $i <= $product->rating ; $i++)
+                        <i class="fa fa-star text-warning"></i>
+                        @endfor
+                        <span> {{ $product->rating }}/5</span>
                       </div>
-                    @endif
-                  @endforeach
+                    </div>
+                    <div class="mt-auto p-3">
+                      <a href="https://wa.me/6281999651534" class="btn btn-warning fw-bold" style="width:100%"><i class="fab fa-whatsapp"></i> Pesan Sekarang</a>
+                    </div>
+                  </div>
                 </div>
+              </div>
+              @endforeach
             </div>
-            @else
-            <div class="container pt-2 pb-5">
-              <p>Product Belum ditambahkan</p>
-            </div>
-            @endif
+          </div>
+          @else
+          <div class="container pt-2 pb-5">
+            <p>Product Belum ditambahkan</p>
+          </div>
+          @endif
         </section>
+        
         <section id="about">
             <!-- Footer -->
             <footer class="text-center text-lg-start bg-dark text-white">
