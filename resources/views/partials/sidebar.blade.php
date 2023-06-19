940589335e8a890aa146b5ebd398414559c71ad5
AnandaCahyaRamadan <76020880+AnandaCahyaRamadan@users.noreply.github.com>
2023-06-19 16:44:18 +0700

    [FIX] Sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -10,40 +10,40 @@
                       Dashboard
                   </a>
                   @endif
-                  <a class="nav-link {{ Request::is('sliders') ? 'active' : '' }}" href="/sliders">
+                  <a class="nav-link {{ Request::is('sliders*') ? 'active' : '' }}" href="/sliders">
                     <div class="sb-nav-link-icon"><i class="fas fa-image"></i></div>
                     Slider
                   </a>
 
                   <div class="sb-sidenav-menu-heading">Management</div>
                   @if (Auth::user()->roles->role_name == 'admin')
-                  <a class="nav-link {{ Request::is('categories') || Request::is('products') ? 'active' : '' }} collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts1" aria-expanded="false" aria-controls="collapseLayouts">
+                  <a class="nav-link {{ Request::is('categories*') || Request::is('products*') ? 'active' : '' }} collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts1" aria-expanded="false" aria-controls="collapseLayouts">
                       <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                       Product
                       <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                   </a>
                   <div class="collapse" id="collapseLayouts1" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                       <nav class="sb-sidenav-menu-nested nav">
-                          <a class="nav-link {{ Request::is('categories') ? 'active' : '' }}" href="/categories">Category</a>
-                          <a class="nav-link {{ Request::is('products') ? 'active' : '' }}" href="/products">Products</a>
+                          <a class="nav-link {{ Request::is('categories*') ? 'active' : '' }}" href="/categories">Category</a>
+                          <a class="nav-link {{ Request::is('products*') ? 'active' : '' }}" href="/products">Products</a>
                       </nav>
                   </div>
                   @else
-                  <a class="nav-link {{ Request::is('products') ? 'active' : '' }}" href="/products">
+                  <a class="nav-link {{ Request::is('products*') ? 'active' : '' }}" href="/products">
                     <div class="sb-nav-link-icon"><i class="fas fa-shopping-cart"></i></div>
                     Product
                   </a>
                   @endif
                   @can('admin')
-                  <a class="nav-link {{ Request::is('roles') || Request::is('users') ? 'active' : '' }}  collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                  <a class="nav-link {{ Request::is('roles*') || Request::is('users*') ? 'active' : '' }}  collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                       <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                       User
                       <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                   </a>
                   <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                       <nav class="sb-sidenav-menu-nested nav">
-                          <a class="nav-link {{ Request::is('roles') ? 'active' : '' }}" href="/roles">Role</a>
-                          <a class="nav-link {{ Request::is('users') ? 'active' : '' }}" href="{{ route('users.index')}}">Tambah user</a>
+                          <a class="nav-link {{ Request::is('roles*') ? 'active' : '' }}" href="/roles">Role</a>
+                          <a class="nav-link {{ Request::is('users*') ? 'active' : '' }}" href="{{ route('users.index')}}">Tambah user</a>
                       </nav>
                   </div>
                   @endcan
